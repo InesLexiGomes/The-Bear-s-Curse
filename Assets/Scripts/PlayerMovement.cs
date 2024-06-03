84cf1cf3d475cc22b14d1c07e1028de82ec050db
Inês Gomes <inesgomes.pessoal05@gmail.com>
2024-06-03 10:45:55 +0100

    Commented and reorganized bits of code.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -26,27 +26,29 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        // sets currentVelocity to the velocity of the player at that instant as a "default"
         currentVelocity = rb.velocity;
 
+        // Determines velocity based on a fixed speed value and the horizontal movement input
         deltaX = Input.GetAxis("Horizontal");
-
         currentVelocity.x = deltaX * speed;
 
+        // While the player is grounded they can jump
         if ((Input.GetButtonDown("Jump")) && IsGrounded())
         {
             currentVelocity.y = jumpSpeed;
             rb.gravityScale = 1.0f;
             jumpTime = Time.time;
         }
+        // Add a bit more to the jump if the player holds the jump button after jumping
         else if ((Input.GetButton("Jump")) && ((Time.time - jumpTime) < maxJumpTime))
         {
             rb.gravityScale = 1.0f;
         }
-        else
-        {
-            rb.gravityScale = defaultGravity;
-        }
+        // Revert gravity to default
+        else rb.gravityScale = defaultGravity;
 
+        // Finally convert the currentVelocity into the velocity of the player
         rb.velocity = currentVelocity;
 
         // When walking backwards flip
@@ -55,18 +57,12 @@ public class PlayerMovement : MonoBehaviour
         else if (rb.velocity.x > 0) transform.rotation = Quaternion.identity;
     }
 
-    private void FixedUpdate()
-    {
-        if (Input.GetKeyDown("up") && IsGrounded())
-        {
-            rb.velocity = new Vector2(deltaX * speed, jumpSpeed);
-        }
-    }
-
     private bool IsGrounded()
     {
+        // Checks for an overlap in the groundCheck and Ground Colliders
         Collider2D collider = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundCheckLayers);
 
+        // If the collider exists (isn't null) returns true, otherwise returns false
         return (collider != null);
     }
 
