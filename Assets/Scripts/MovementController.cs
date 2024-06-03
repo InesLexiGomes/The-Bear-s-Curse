260377c52d7d63308ae3865743f764e550423640
Inês Gomes <inesgomes.pessoal05@gmail.com>
2024-06-03 09:56:52 +0100

    Updated protection level in the player's movement script

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -4,29 +4,30 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class MovementController : MonoBehaviour
 {
-    float move;
-    public int speed;
+    private float move;
+    [SerializeField]
+    private int speed;
 
     Rigidbody2D rb;
 
-    void Start()
+    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    private void Update()
     {
         move = Input.GetAxis("Horizontal");
 
         Flip();
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         rb.velocity = new Vector2(move*speed, rb.velocity.y);
     }
 
-    void Flip()
+    private void Flip()
     {
         // When walking backwards flip
         if (rb.velocity.x < 0) transform.rotation = Quaternion.Euler(0, 180, 0);
